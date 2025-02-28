0e98b9b975652012e7d170f4521fdb74ab78efa9
Christian Roy <christian@vi.co>
2025-02-28 10:24:49 -0500

    style(eslint): Enforce no-trailing-spaces rule
    Added "no-trailing-spaces" rule in the ESLint configuration to ensure
    consistent code formatting and improve readability. Trailing spaces
    can lead to unnecessary diffs and confusion during code reviews. This
    change highlights inconsistencies and enforces cleaner coding
    practices by automatically raising an error when trailing spaces
    are detected.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,12 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+    {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+    {languageOptions: { globals: globals.browser }},
+    pluginJs.configs.recommended,
+    {
+        rules: {
+            "no-trailing-spaces": "error"
+        }
+    }
 ];
